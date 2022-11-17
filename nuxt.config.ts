bd920a33bb0190b207da8c603c303110b56f7f6f
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-11-17 11:01:46 +0100

    add API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    modules: [
+      [
+        '@storyblok/nuxt',
+        {
+                  accessToken: 'Ye2Tu63nTUUbFfxQJOPqDAtt',
+                  // If Server Location === US then:
+                  // apiOptions: { region: 'us' }
+              }
+      ]
+    ],
+  })
